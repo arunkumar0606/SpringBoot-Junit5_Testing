e94a21bd6ff657e581610ce058b37cddfc4dd80f
Arun <arunkumard0606@gmail.com>
2024-06-27 10:01:54 +0530

    Initial commit

---
+++ src/main/java/com/example/Junit5_testing/Junit5TestingApplication.java
@@ -0,0 +1,14 @@
+package com.example.Junit5_testing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Junit5TestingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Junit5TestingApplication.class, args);
+	System.out.println("Hello");
+	}
+
+}
